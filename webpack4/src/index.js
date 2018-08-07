8bbef9f96aae5eb516376127124f0cdab5837f05
laolaolao <1317046596@qq.com>
2018-08-07 16:02:07 +0800

    webpack

---
+++ webpack4/src/index.js
@@ -0,0 +1,5 @@
+import css from './css/index.css';
+let oDiv1 = document.querySelector('#div1');
+oDiv1.innerHTML = 'Hello Wolrd index1';
+let sum = (x,y) =>x+y;
+$('#div3').html(`作者是：${author.name}, 公司是: ${author.company}`);
